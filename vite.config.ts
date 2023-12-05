b7a95b59c3a44b5201b56c0bd0d3e58fbb1f2853
HYEOK9 <leejaehyuck9@gmail.com>
2023-12-05 13:08:13 +0900

    __version

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
@@ -7,4 +8,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: { port: 3000 },
   plugins: [react({ tsDecorators: true }), svgr(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+    },
+  },
 });
